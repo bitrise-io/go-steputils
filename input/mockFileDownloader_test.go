60b5a8ec55fe8a2b5ac1ea2bcbcb42c3e136d116
lpusok <7979773+lpusok@users.noreply.github.com>
2021-09-29 18:21:40 +0200

    Adding support for downloading file contents (#38)

--- input/mockFileDownloader_test.go
+++ input/mockFileDownloader_test.go
@@ -15,6 +15,28 @@ func (m *MockFileDownloader) Get(destination, source string) error {
 	return args.Error(0)
 }
 
+// GetContents ...
+func (m *MockFileDownloader) GetRemoteContents(source string) ([]byte, error) {
+	args := m.Called(source)
+	arg0, ok := args.Get(0).([]byte)
+	if !ok {
+		panic("unexpected type")
+	}
+
+	return arg0, args.Error(1)
+}
+
+// ReadLocalFile ...
+func (m *MockFileDownloader) ReadLocalFile(path string) ([]byte, error) {
+	args := m.Called(path)
+	arg0, ok := args.Get(0).([]byte)
+	if !ok {
+		panic("unexpected type")
+	}
+
+	return arg0, args.Error(1)
+}
+
 // GivenGetFails ...
 func (m *MockFileDownloader) GivenGetFails(reason error) *MockFileDownloader {
 	m.On("Get", mock.Anything, mock.Anything).Return(reason)
