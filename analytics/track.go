1d7d51d6e38ea60bc8d3ccd70ec923bdb1b92496
Mate Herber <mate.herber@bitrise.io>
2022-04-07 17:58:05 +0200

    Fix `errcheck` errors

--- analytics/track.go
+++ analytics/track.go
@@ -7,21 +7,24 @@ import (
 	"github.com/bitrise-io/go-utils/v2/env"
 )
 
+// TrackerFactory ...
 type TrackerFactory func(...analytics.Properties) analytics.Tracker
 
 const (
-	StepExecutionIDEnvKey = "BITRISE_STEP_EXECUTION_ID"
-	StepExecutionID       = "step_execution_id"
+	stepExecutionIDEnvKey = "BITRISE_STEP_EXECUTION_ID"
+	stepExecutionID       = "step_execution_id"
 )
 
+// NewStepTracker ...
 func NewStepTracker(repository env.Repository, trackerFactory TrackerFactory) (analytics.Tracker, error) {
-	stepExecutionID := repository.Get(StepExecutionIDEnvKey)
-	if stepExecutionID == "" {
+	id := repository.Get(stepExecutionIDEnvKey)
+	if id == "" {
 		return nil, fmt.Errorf("no step execution ID found")
 	}
-	return trackerFactory(analytics.Properties{StepExecutionID: stepExecutionID}), nil
+	return trackerFactory(analytics.Properties{stepExecutionID: id}), nil
 }
 
+// NewDefaultStepTracker ...
 func NewDefaultStepTracker(repository env.Repository) (analytics.Tracker, error) {
 	return NewStepTracker(repository, analytics.NewDefaultTracker)
 }
