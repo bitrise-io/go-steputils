d5da4f2de03a0c0ba18e1622b5580624c8bab697
lpusok <7979773+lpusok@users.noreply.github.com>
2022-11-02 15:00:21 +0100

    Update analytics/track.go
    Co-authored-by: Olivér Falvai <oliver.falvai@bitrise.io>

--- analytics/track.go
+++ analytics/track.go
@@ -15,7 +15,7 @@ const (
 	stepExecutionID       = "step_execution_id"
 )
 
-// NewStepTracker ...
+// NewStepTracker creates a tracker that adds the `step_execution_id` property to all events logged with this instance. This is useful for joining the logged data with other tables on the execution ID.
 func NewStepTracker(repository env.Repository, trackerFactory TrackerFactory) (analytics.Tracker, error) {
 	id := repository.Get(stepExecutionIDEnvKey)
 	if id == "" {
