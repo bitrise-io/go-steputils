468574625a0a96e588888675575095c798c25909
tatiana <tatiana@flare.build>
2023-12-03 19:35:11 -0800

    reduced time between attempts

--- cache/network/download.go
+++ cache/network/download.go
@@ -51,12 +51,13 @@ func Download(ctx context.Context, params DownloadParams, logger log.Logger) (ma
 	logger.Debugf("Download archive")
 
 	const maxRetries = 3
-	const retryDelay = 2 * time.Second
+	const retryDelay = 1 * time.Second
 	retriableErrors := []string{"Range request returned invalid Content-Length", "EOF"}
 
 	// Attempt to download, retrying on retriable errors
 	for attempt := 0; attempt < maxRetries; attempt++ {
 		downloadErr := downloadFile(ctx, retryableHTTPClient.StandardClient(), restoreResponse.URL, params.DownloadPath)
+
 		if downloadErr == nil {
 			return restoreResponse.MatchedKey, nil
 		}
