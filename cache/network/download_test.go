903224b2f5969e150479731c915221e8bb31b486
Viktor Benei <viktorbenei@bitrise.io>
2023-12-22 17:43:19 +0100

    Test_downloadFile - multi part test server

--- cache/network/download_test.go
+++ cache/network/download_test.go
@@ -3,8 +3,12 @@ package network
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/bitrise-io/go-utils/v2/mocks"
@@ -87,9 +91,7 @@ func Test_downloadFile(t *testing.T) {
 	mockLogger := new(mocks.Logger)
 	mockLogger.On("Debugf", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return()
 
-	downloadURL := "https://github.com/bitrise-io/bitrise/releases/download/2.6.1/bitrise-Linux-x86_64"
 	retryableHTTPClient := retryhttp.NewClient(mockLogger)
-
 	isCheckRetryCalled := false
 	retryFunc := func(ctx context.Context, resp *http.Response, downloadErr error) (bool, error) {
 		retry, err := retryablehttp.DefaultRetryPolicy(ctx, resp, downloadErr)
@@ -97,14 +99,58 @@ func Test_downloadFile(t *testing.T) {
 		return retry, err
 	}
 	retryableHTTPClient.CheckRetry = retryFunc
+
 	tmpPath := t.TempDir()
 	tmpFile := filepath.Join(tmpPath, "testfile.bin")
+	testDummyFileContent := strings.Repeat("a", 1024*1024*10) // 10MB
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server called. Method=%s; Header=%#v", r.Method, r.Header)
+		rangeHeader := r.Header.Get("Range")
+		if len(rangeHeader) < 1 {
+			t.Fatal("No Range header found")
+		}
+
+		if !strings.HasPrefix(rangeHeader, "bytes=") {
+			t.Fatalf("invalid range header: should start with 'bytes=' ; actual range header value was=%s", rangeHeader)
+		}
+		rangeHeader = strings.TrimPrefix(rangeHeader, "bytes=")
+		rangeHeaderFromTo := strings.Split(rangeHeader, "-")
+		if len(rangeHeaderFromTo) != 2 {
+			t.Fatalf("invalid range header: invalid from-to value. Range header value was=%s", rangeHeader)
+		}
+		rangeHeaderFrom, err := strconv.ParseUint(rangeHeaderFromTo[0], 10, 64)
+		require.NoError(t, err)
+		rangeHeaderTo, err := strconv.ParseUint(rangeHeaderFromTo[1], 10, 64)
+		require.NoError(t, err)
+
+		if rangeHeaderFrom == 0 && rangeHeaderTo == 0 {
+			// range request - requesting content size - return the size info
+			w.Header().Add("content-range", fmt.Sprintf("bytes 0-0/%d", len(testDummyFileContent)))
+			_, err := fmt.Fprint(w, " ")
+			require.NoError(t, err)
+		} else {
+			// actual content chunk request - return chunk content
+			chunkContent := testDummyFileContent[rangeHeaderFrom : rangeHeaderTo+1]
+			// We also have to set the Content-Length header manually due to the size of the response.
+			// From the documentation of http.ResponseWriter:
+			// > ... if the total size of all written
+			// > data is under a few KB and there are no Flush calls, the
+			// > Content-Length header is added automatically.
+			w.Header().Add("Content-Length", fmt.Sprintf("%d", len(chunkContent)))
+			_, err := fmt.Fprint(w, chunkContent)
+			require.NoError(t, err)
+		}
+	}))
+	defer svr.Close()
+	// downloadURL := "https://github.com/bitrise-io/bitrise/releases/download/2.6.1/bitrise-Linux-x86_64"
+	downloadURL := svr.URL
 
 	// When
 	err := downloadFile(context.Background(), retryableHTTPClient.StandardClient(), downloadURL, tmpFile)
 
 	// Then
-	require.NoError(t, err)
 	require.True(t, isCheckRetryCalled)
+	require.NoError(t, err)
 	mockLogger.AssertExpectations(t)
 }
