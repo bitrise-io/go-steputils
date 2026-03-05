40db75dc3143db48b2b20702167712995a5715af
ramil-bitrise <112068385+ramil-bitrise@users.noreply.github.com>
2026-03-05 15:29:10 +0200

    ACI-4580 Export chunk downloader (#135)

--- cache/network/download.go
+++ cache/network/download.go
@@ -4,16 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
-	"net/http"
-	"os"
-	"strconv"
 	"time"
 
+	"github.com/bitrise-io/go-steputils/v2/cache/network/chunkdownloader"
 	"github.com/bitrise-io/go-utils/retry"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-utils/v2/retryhttp"
-	"github.com/bitrise-io/got"
 	"github.com/hashicorp/go-retryablehttp"
 )
 
@@ -93,54 +89,10 @@ func downloadWithClient(ctx context.Context, httpClient *retryablehttp.Client, p
 }
 
 func downloadFile(ctx context.Context, httpClient *retryablehttp.Client, url string, dest string, maxConcurrency uint, logger log.Logger) error {
-	env := os.Getenv("BITRISEIO_DEPENDENCY_CACHE_MAX_IDLE_CONNS_PER_HOST")
-	maxIdleConnsPerHost, err := strconv.Atoi(env)
-	if err == nil {
-		httpClient.HTTPClient.Transport.(*http.Transport).MaxIdleConnsPerHost = maxIdleConnsPerHost
-	}
+	cfg := chunkdownloader.DefaultConfig()
+	cfg.Concurrency = maxConcurrency
+	cfg.HTTPClient = httpClient.StandardClient()
 
-	env = os.Getenv("BITRISEIO_DEPENDENCY_CACHE_MAX_IDLE_CONNS")
-	maxIdleConns, err := strconv.Atoi(env)
-	if err == nil {
-		httpClient.HTTPClient.Transport.(*http.Transport).MaxIdleConns = maxIdleConns
-	}
-
-	env = os.Getenv("BITRISEIO_DEPENDENCY_CACHE_FORCE_ATTEMPT_HTTP2")
-	forceAttemptHTTP2 := env == "true" || env == "1"
-	httpClient.HTTPClient.Transport.(*http.Transport).ForceAttemptHTTP2 = forceAttemptHTTP2
-
-	env = os.Getenv("BITRISEIO_DEPENDENCY_CACHE_DUALSTACK")
-	dualStack := env == "true" || env == "1"
-	httpClient.HTTPClient.Transport.(*http.Transport).DialContext = (&net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-		DualStack: dualStack,
-	}).DialContext
-
-	downloader := got.New()
-	downloader.Client = httpClient.StandardClient()
-
-	gDownload := got.NewDownload(ctx, url, dest)
-	// Client has to be set on "Download" as well,
-	// as depending on how downloader is called
-	// either the Client from the downloader or from the Download will be used.
-	gDownload.Client = httpClient.StandardClient()
-	gDownload.Concurrency = maxConcurrency
-	gDownload.Logger = logger
-
-	env = os.Getenv("BITRISEIO_DEPENDENCY_CACHE_MAX_RETRY_PER_CHUNK")
-	if val, err := strconv.Atoi(env); err == nil {
-		gDownload.MaxRetryPerChunk = val
-	} else {
-		gDownload.MaxRetryPerChunk = 5
-	}
-
-	env = os.Getenv("BITRISEIO_DEPENDENCY_CACHE_CHUNK_RETRY_THRESHOLD")
-	if val, err := strconv.Atoi(env); err == nil {
-		gDownload.ChunkRetryThreshold = time.Duration(val) * time.Second
-	} else {
-		gDownload.ChunkRetryThreshold = 10 * time.Second
-	}
-
-	return downloader.Do(gDownload)
+	dl := chunkdownloader.New(cfg, logger)
+	return dl.DownloadFile(ctx, url, dest)
 }
