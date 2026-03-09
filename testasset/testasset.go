9f2b95e8409f04d0abe49b166d08353938335a7a
Gergely Sallai <1516628+gergely-sallai@users.noreply.github.com>
2026-03-09 11:53:46 +0100

    style: STEP-2230 Fix lint issues

--- testasset/testasset.go
+++ testasset/testasset.go
@@ -1,3 +1,4 @@
+// Package testasset provides helpers for filtering test result attachments by file type.
 package testasset
 
 import (
@@ -6,8 +7,10 @@ import (
 	"strings"
 )
 
+// AssetTypes is the list of supported attachment file extensions.
 var AssetTypes = []string{".jpg", ".jpeg", ".png", ".txt", ".log", ".mp4", ".webm", ".ogg"}
 
+// IsSupportedAssetType reports whether the given file name has a supported attachment extension.
 func IsSupportedAssetType(fileName string) bool {
 	ext := filepath.Ext(fileName)
 	return slices.Contains(AssetTypes, strings.ToLower(ext))
